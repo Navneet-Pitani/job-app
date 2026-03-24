68f70d443aa17ca2ae44fecabcf9e0d054e66d69
Navneet Pitani <navneetpitani@gmail.com>
2026-03-24 16:46:50 +0530

    fix vercel routing

--- frontend/src/features/jobs/jobsSlice.js
+++ frontend/src/features/jobs/jobsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-const API = 'http://localhost:5000/api'
+const API = 'https://job-app-p42e.onrender.com/api'
 
 const getToken = (thunkAPI) => {
   return thunkAPI.getState().auth.user?.token
